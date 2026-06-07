6b2b7367c1bc774f352812845cde72c0e8440bde
caique mancini <caique.mancini@exemplo.com>
2026-06-06 23:04:27 -0300

    criado função para salvar os dados do produto no banco de dados, e adicionado a chamada dessa função na página de cadastro do produto. Além disso, adicionei a importação do módulo sqlite3 para trabalhar com o banco de dados.

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import sqlite3
+from pathlib import Path
+
+db_path = Path(__file__).parent.parent / "database" / "database.db"
 
 st.title("Cadastro de Produto")
 
@@ -9,4 +13,33 @@ version = st.text_input("Versão")
 family = st.text_input("Família")
 
 if st.button("Salvar"):
-    st.success("Botão funcionando!")
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO product (name, version, family)
+        VALUES (?, ?, ?)
+    """, (name, version, family))
+
+    conn.commit()
+    conn.close()
+
+    st.success("Produto cadastrado com sucesso!")
+
+    st.subheader("Produtos cadastrados")
+
+conn = sqlite3.connect(db_path)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM product")
+
+dados = cursor.fetchall()
+
+conn.close()
+
+st.dataframe(
+    dados,
+    use_container_width=True
+)
